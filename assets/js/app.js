ddb6d528a11fbaff9d429736f3d0a86eaa3bed50
junaid moda <jmoda1028@gmail.com>
2023-01-06 01:07:58 +0800

    solution

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,14 @@
-const share = document.getElementById('share');
+
 const shareIcon = document.querySelector(".share-icon");
+const section = document.querySelector('section');
 
 shareIcon.addEventListener('click', () => {
-    if (share.style.display === 'none') {
-        share.style.display = 'flex'
-    } else {
-        share.style.display = 'none'
-    }
+    
+    section.classList.toggle('shares');
+
+    // if (share.style.display === 'none') {
+    //     share.style.display = 'flex'
+    // } else {
+    //     share.style.display = 'none'
+    // }
   });
